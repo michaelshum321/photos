fc467131de7ba9877d8e00b74dc9a93092f37f23
Michael Shum <mckshum@gmail.com>
2023-10-05 01:41:04 -0700

    wasdf

--- src/server/repos/FileRepo.ts
+++ src/server/repos/FileRepo.ts
@@ -1,14 +1,17 @@
 import { promises as fs } from 'fs';
 import { type Stream } from 'stream';
 
+const DB_PATH = '/stuff/';
 const DEFAULT_PATH = './public/stuff/';
 
 class FileRepo {
-    async create(contents: Stream) {
+    async create(contents: Buffer, extension: string) {
         await this.mkdir();
-        const path = String.fromCharCode(Math.floor(Math.random() * 26) + 'a'.charCodeAt(0));
-        await fs.writeFile(`${DEFAULT_PATH}${path}`, contents);
-        return path;
+
+        const filename = `${this.randomPath()}${extension}`;
+        const path =`${DEFAULT_PATH}${filename}`
+        await fs.writeFile(path, contents);
+        return `${DB_PATH}${filename}`;
     }
 
     async mkdir() {
@@ -21,7 +24,11 @@ class FileRepo {
     }
 
     async read(path: string) {
-        await fs.readFile(path);
+        return await fs.readFile(path);
+    }
+    private randomPath(): string {
+        const randomChar = () => String.fromCharCode(Math.floor(Math.random() * 26) + 'a'.charCodeAt(0));
+        return new Array(6).fill(null).map(randomChar).join('');
     }
 }
 
